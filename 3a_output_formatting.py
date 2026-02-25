72ad32248655ddea19a0c2d7a3161223122fa553
Lamija Karic <lamynessa23@gmail.com>
2026-02-25 04:45:33 -0500

    Add 3a_output_formatting for Task 3 Activity 1

---
+++ 3a_output_formatting.py
@@ -0,0 +1,32 @@
+import requests
+
+def emotion_detector(text_to_analyze):
+
+    url = "https://sn-watson-emotion.labs.skills.network/emotionDetector"
+
+    myobj = { "text": text_to_analyze }
+
+    response = requests.post(url, json = myobj)
+
+    formatted_response = response.json()
+
+    emotions = formatted_response["emotionPredictions"][0]["emotion"]
+
+    anger = emotions["anger"]
+    disgust = emotions["disgust"]
+    fear = emotions["fear"]
+    joy = emotions["joy"]
+    sadness = emotions["sadness"]
+
+    dominant_emotion = max(emotions, key=emotions.get)
+
+    result = {
+        "anger": anger,
+        "disgust": disgust,
+        "fear": fear,
+        "joy": joy,
+        "sadness": sadness,
+        "dominant_emotion": dominant_emotion
+    }
+
+    return result
