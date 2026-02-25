a2b38acf33edb0ba82f84a875465f280602f5fd3
lamyprojects <lamynessa23@gmail.com>
2026-02-25 21:18:53 +0000

    save

--- static/mywebscript.js
+++ static/mywebscript.js
@@ -1,14 +1,14 @@
 function RunSentimentAnalysis() {
-  const textToAnalyze = document.getElementById("textToAnalyze").value;
 
-  const xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function () {
-    if (this.readyState === 4) {
-      document.getElementById("system_response").innerHTML = this.responseText;
-    }
-  };
+    const textToAnalyze = document.getElementById("textToAnalyze").value;
 
-  const url = "/emotionDetector?textToAnalyze=" + encodeURIComponent(textToAnalyze);
-  xhttp.open("GET", url, true);
-  xhttp.send();
+    fetch("emotionDetector?textToAnalyze=" + encodeURIComponent(textToAnalyze))
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("system_response").innerHTML = data;
+        })
+        .catch(error => {
+            document.getElementById("system_response").innerHTML =
+                "Error: " + error;
+        });
 }
